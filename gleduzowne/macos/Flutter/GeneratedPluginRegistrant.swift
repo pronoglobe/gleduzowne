cf5a0ced92d77b7ffc270e3dba85e527120cc63a
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-10-07 10:33:47 +0400

    testing up

--- gleduzowne/macos/Flutter/GeneratedPluginRegistrant.swift
+++ gleduzowne/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import ffmpeg_kit_flutter
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
