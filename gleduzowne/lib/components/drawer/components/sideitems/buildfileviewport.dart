c0f4d0d0df963565527666fd753c9a69d2955471
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-10-18 18:37:56 +0400

    from pronoglobe

--- gleduzowne/lib/components/drawer/components/sideitems/buildfileviewport.dart
+++ gleduzowne/lib/components/drawer/components/sideitems/buildfileviewport.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+import 'package:video_player/video_player.dart';
 
 class FileExplorer extends StatefulWidget {
   const FileExplorer({super.key});
@@ -13,6 +14,7 @@ class FileExplorer extends StatefulWidget {
 class _FileExplorerState extends State<FileExplorer> {
   late Directory _currentDirectory;
   List<FileSystemEntity> _files = [];
+  VideoPlayerController? _videoController;
 
   @override
   void initState() {
@@ -36,14 +38,49 @@ class _FileExplorerState extends State<FileExplorer> {
   }
 
   void _openFile(FileSystemEntity file) {
-    // Logic to open the file
-    // If unable to open, show error overlay
+    if (file is File) {
+      final extension = file.path.split('.').last.toLowerCase();
+      if (['jpg', 'jpeg', 'png', 'gif'].contains(extension)) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              child: Image.file(file),
+            );
+          },
+        );
+      } else if (['mp4', 'mov', 'avi'].contains(extension)) {
+        _videoController = VideoPlayerController.file(file)
+          ..initialize().then((_) {
+            setState(() {});
+            _videoController!.play();
+          });
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              child: _videoController!.value.isInitialized
+                  ? AspectRatio(
+                aspectRatio: _videoController!.value.aspectRatio,
+                child: VideoPlayer(_videoController!),
+              )
+                  : const Center(child: CircularProgressIndicator()),
+            );
+          },
+        );
+      } else {
+        _showErrorDialog('Unable to open file.');
+      }
+    }
+  }
+
+  void _showErrorDialog(String message) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: const Text('Error'),
-          content: const Text('Unable to open file.'),
+          content: Text(message),
           actions: <Widget>[
             TextButton(
               child: const Text('OK'),
@@ -57,6 +94,22 @@ class _FileExplorerState extends State<FileExplorer> {
     );
   }
 
+  void _createNewFolder() {
+    // Logic to create a new folder
+  }
+
+  void _createNewFile() {
+    // Logic to create a new file
+  }
+
+  void _deleteFileOrFolder(FileSystemEntity entity) {
+    // Logic to delete a file or folder
+  }
+
+  void _renameFileOrFolder(FileSystemEntity entity) {
+    // Logic to rename a file or folder
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,6 +119,10 @@ class _FileExplorerState extends State<FileExplorer> {
           IconButton(
             icon: const Icon(Icons.refresh),
             onPressed: _reloadFiles,
+          ),
+          IconButton(
+            icon: const Icon(Icons.create_new_folder),
+            onPressed: _createNewFolder,
           ),
         ],
       ),
@@ -102,6 +159,42 @@ class _FileExplorerState extends State<FileExplorer> {
           return ListTile(
             title: Text(file.path.split('/').last),
             onTap: () => _openFile(file),
+            onLongPress: () {
+              showMenu(
+                context: context,
+                position: const RelativeRect.fromLTRB(100, 100, 100, 100),
+                items: [
+                  PopupMenuItem(
+                    child: const Text('New Folder'),
+                    onTap: _createNewFolder,
+                  ),
+                  PopupMenuItem(
+                    child: const Text('New File'),
+                    onTap: _createNewFile,
+                  ),
+                  PopupMenuItem(
+                    child: const Text('Delete'),
+                    onTap: () => _deleteFileOrFolder(file),
+                  ),
+                  PopupMenuItem(
+                    child: const Text('Add to Project Assets'),
+                    onTap: () {},
+                  ),
+                  PopupMenuItem(
+                    child: const Text('Copy'),
+                    onTap: () {},
+                  ),
+                  PopupMenuItem(
+                    child: const Text('Paste'),
+                    onTap: () {},
+                  ),
+                  PopupMenuItem(
+                    child: const Text('Rename'),
+                    onTap: () => _renameFileOrFolder(file),
+                  ),
+                ],
+              );
+            },
           );
         },
       ),
